6de0b8e627deb14c73e9245468c15ef19c462206
Lucas Bloise <lucas.bloise@decemberlabs.com>
2022-09-08 16:52:27 -0300

    add proceso model and rename classes

---
+++ src/main/java/Models/Proceso.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models;
+
+/**
+ *
+ * @author lucasbloise
+ */
+public class Proceso {
+    
+    private int tiempoEjecuccion;
+
+    public int getTiempoEjecuccion() {
+        return tiempoEjecuccion;
+    }
+
+    public void setTiempoEjecuccion(int tiempoEjecuccion) {
+        this.tiempoEjecuccion = tiempoEjecuccion;
+    }
+
+    public Proceso(int tiempoEjecuccion) {
+        this.tiempoEjecuccion = tiempoEjecuccion;
+    }
+    
+    
+    
+}
--- src/main/java/main/ProyectoARQLABBLOISEDEVOTO.java
+++ src/main/java/main/ProyectoARQLABBLOISEDEVOTO.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
 
-package com.mycompany.proyecto.arq.lab.bloise.devoto;
+package main;
 import java.util.Scanner;
 /**
  *
