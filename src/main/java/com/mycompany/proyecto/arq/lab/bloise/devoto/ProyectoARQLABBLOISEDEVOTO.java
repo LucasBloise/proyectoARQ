6329d1c04851dad045e9e3276b19123f05f9b687
Lucas Bloise <lucas.bloise@decemberlabs.com>
2022-09-08 16:35:57 -0300

    init project

---
+++ src/main/java/com/mycompany/proyecto/arq/lab/bloise/devoto/ProyectoARQLABBLOISEDEVOTO.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.proyecto.arq.lab.bloise.devoto;
+import java.util.Scanner;
+/**
+ *
+ * @author lucasbloise
+ */
+public class ProyectoARQLABBLOISEDEVOTO {
+
+    public static void main(String[] args) {
+        int tiempoRutinaSistemaOperativo;
+        //1
+        //tiempo rutina de sitema operativo leer
+        //tiempo entrada salida leer
+        //2
+        // carga de los procesos de cada uno en tiempo y cantiodad 
+        //simular dos politicas 
+        //fifo y ron robin
+        //
+        //3 elegir sistema
+        // 6 salir 
+        
+        System.out.println("Ingrese la cantidad de tiempo que llevara este proceso");
+    }
+}
